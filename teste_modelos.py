fb7a01e0859ae46a1efcc165b37548670d335a1f
Igorcg22 <igorcguerini@hotmail.com>
2026-05-14 21:44:29 -0300

    Delete teste_modelos.py

--- teste_modelos.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyCbCdb14coa0r1IIICDp4pd4Ql_OCOs1FE")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
